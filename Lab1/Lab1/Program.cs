4537c0ed3c4b6b4dd1b50bd07d00d0148402a59e
AndrewF1604 <153609088+AndrewF1604@users.noreply.github.com>
2024-03-13 02:26:10 +0100

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,168 @@
+﻿class Restaurant
+{
+    private string[] menu { get; set; }
+    private string name { get; set; }
+    private string address { get; set; }
+    private int rating { get; set; }
+    public Restaurant(string[] _menu, string _name, string _address, int _rating)
+    {
+        
+        menu = _menu;
+        name = _name;
+        address = _address;
+        rating = _rating;
+    }
+
+    public string Open()
+    {
+        return($"{name} is now open!");
+    }
+    public string Close()
+    {
+        return($"{name} is now closed.");
+    }
+    public virtual string CalculateBill(int numPeople, double pricePerPerson)
+    {
+        double bill = numPeople * pricePerPerson;
+        return ($"Your bill is {bill:C}.");
+    }
+}
+class FastFoodRestaurant : Restaurant
+{
+    private int orderNumber { get; set; }
+    private bool driveThru { get; set; }
+
+    public FastFoodRestaurant(string[] _menu ,string _name, string _address, int _rating, int _orderNumber, bool _driveThru) : base(_menu,_name, _address, _rating)
+    {
+        orderNumber = _orderNumber;
+        driveThru = _driveThru;
+    }
+
+    public string TakeOrder(int _orderNumber)
+    {
+        orderNumber = _orderNumber;
+        return ($"Order {orderNumber} has been taken.");
+    }
+    public string ServeOrder()
+    {
+        return($"Order {orderNumber} is ready. Please pick it up at the counter.");
+    }
+    public override string CalculateBill(int numPeople, double pricePerPerson)
+    {
+        double bill = numPeople * pricePerPerson;
+        return($"Your fast food bill is {bill:C}.");
+    }
+}
+class BurgerKing : FastFoodRestaurant
+{
+    private bool freeRefill { get; set; }
+    private bool coupon { get; set; }
+
+    public BurgerKing(string[] _menu, string _name, string _address, int _rating, int _orderNumber, bool _driveThru, bool _freeRefill,bool _coupon) : base(_menu, _name, _address, _rating, _orderNumber, _driveThru)
+    {
+        freeRefill = _freeRefill;
+        coupon = _coupon;
+    }
+
+    public double AddCoupon(bool Coupon, double totalPrice)
+    {
+        if (Coupon)
+        {
+            double discountedPrice = totalPrice * 0.9;
+            return discountedPrice;
+        }
+        else
+        {
+            return totalPrice;
+        }
+    }
+}
+    class ItalianRestaurant : Restaurant
+{
+    private bool reservations { get; set; }
+    private bool wineList { get; set; }
+    public ItalianRestaurant(string[] _menu, string _name, string _address, int _rating, bool _reservations, bool _wineList) : base(_menu, _name, _address, _rating)
+    {
+        reservations = _reservations;
+        wineList = _wineList;
+    }
+
+    public static string MakeReservation()
+    {
+        return("Your reservation has been made.");
+    }
+    public override string CalculateBill(int numPeople, double pricePerPerson)
+    {
+        double bill = numPeople * pricePerPerson;
+        double tip = bill * 0.15;
+        return($"Your Italian restaurant bill is {bill:C}. Don't forget to leave a tip of {tip:C}!");
+    }
+}
+class PizzaRestaurant : ItalianRestaurant
+{
+
+    private string[] toppings { get; set; }
+    private bool delivery { get; set; }
+    private bool takeout { get; set; }
+    public PizzaRestaurant(string[] _menu, string _name, string _address, int _rating, bool _reservations, bool _wineList, string[] _toppings, bool _delivery,bool _takeout) : base(_menu, _name, _address, _rating, _reservations, _wineList)
+    {
+        toppings = _toppings;
+        delivery = _delivery;
+        takeout = _takeout;
+    }
+
+    public string OrderPizza(string[] toppings)
+    {
+        return($"One {string.Join(" and ", toppings)} pizza coming right up!");
+    }
+    public string ScheduleDelivery()
+    {
+        return("Your pizza will be delivered in 30 minutes or less.");
+    }
+
+    public override string CalculateBill(int numPeople, double pricePerPerson)
+    {
+        double bill = numPeople * pricePerPerson;
+        double tip = bill * 0.20;
+        return($"Your pizza restaurant bill is {bill:C}. Don't forget to leave a tip of {tip:C}!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] bkMenu = { "Whopper", "Chicken Fries", "Big Mac" };
+        BurgerKing bk = new BurgerKing(bkMenu, "Burger King", "Czarnowiejska 69", 4, 1, true, true, true);
+        Console.WriteLine(bk.Open());
+        Console.WriteLine(bk.TakeOrder(1));
+        Console.WriteLine(bk.ServeOrder());
+        double totalPrice = 15.0;
+        Console.WriteLine($"Total price before coupon: {totalPrice:C}");       //BURGER KING
+        double discountedPrice = bk.AddCoupon(true, totalPrice);
+        Console.WriteLine($"Total price after coupon: {discountedPrice:C}");
+        Console.WriteLine(bk.Close());
+
+        Console.WriteLine();
+
+
+        string[] italianMenu = { "Spaghetti Carbonara", "Margherita Pizza", "Tiramisu" };
+        ItalianRestaurant italian = new ItalianRestaurant(italianMenu, "Parmigiano Rigiano", "Lea 666", 5, true, true);
+        Console.WriteLine(italian.Open());
+        Console.WriteLine(ItalianRestaurant.MakeReservation());                     //ITALIAN
+        int numPeople = 4;
+        double pricePerPerson = 20.0;
+        Console.WriteLine(italian.CalculateBill(numPeople, pricePerPerson));
+        Console.WriteLine(italian.Close());
+
+        Console.WriteLine();
+
+        string[] pizzaToppings = { "pepperoni", "mushrooms", "onions" };
+        PizzaRestaurant pizza = new PizzaRestaurant(italianMenu, "Dominium", "Nowy kleparz 42", 4, false, true, pizzaToppings, true, true);
+        Console.WriteLine(pizza.Open());
+        Console.WriteLine(pizza.OrderPizza(pizzaToppings));              //PIZZA
+        Console.WriteLine(pizza.ScheduleDelivery());
+        Console.WriteLine(pizza.CalculateBill(numPeople, pricePerPerson));
+        Console.WriteLine(pizza.Close());
+    }
+
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,209 @@
+﻿interface IInternetConnection {
+     abstract bool InternetConnection();
+}
+abstract class Robot
+{
+    private string Type;
+    public Robot(string type)
+    {
+        Type = type;
+    }
+    public abstract int AnnualCost();
+}
+class ComputerRobot : Robot, IInternetConnection
+{
+    private bool internetConnection;
+    //public bool InternetConnection { get; set; }
+    public bool InternetConnection()
+    {
+        return internetConnection;
+    }
+    public ComputerRobot(bool _InternetConnection) : base("Computer")
+    {
+        internetConnection = _InternetConnection;
+    }
+public override int AnnualCost()
+    {
+        if (internetConnection) { return 10000; }
+        else return 5000;
+        
+    }
+}
+
+class MechanicalRobot : Robot
+{
+    public MechanicalRobot() : base("Mechanical")
+    {
+    }
+    public override int AnnualCost()
+    {
+        return 20000;
+    }
+}
+abstract class Human
+{
+    private string name { get; set; }
+    public string Name { get { return name; } }
+    private int months;
+    //public int Month { get { return months; }  } 
+    public int Months { get; set; }
+
+    public Human(string _name,int _months)
+        {
+            name = _name;
+            Months = _months;
+        }
+    public virtual string To_String()
+    {
+        return ($"My name is {Name:C} working for {Months:C}");
+    }
+    public abstract int Cost(int months);   
+}
+class Handworker : Human
+{
+    public  override string To_String()
+    {
+        return ($"My name is {Name} working for {Months} as Handworker");
+    }
+    public Handworker(string _name,int _months):base (_name,_months)
+        {
+        }
+    public override int Cost(int months)
+    {
+        return months*3000+500;
+    }
+}
+class OfficeWorker : Human, IInternetConnection
+{
+    private bool internetConnection;
+    public bool InternetConnection()
+    {
+        return internetConnection;
+    }
+    public OfficeWorker(string _name, int _months,bool _InternetConnection) : base(_name, _months)
+    {
+        internetConnection = _InternetConnection;
+    }
+    public override int Cost(int months)
+    {
+        return months*2500;
+    }
+    public override string To_String()
+    {
+        return ($"My name is {Name} working for {Months} as OfficeWorker. Connection:{internetConnection}");
+    }
+}
+class Factory
+{
+private List<Robot> _robotsList = new List<Robot>();
+private List<Human> _humansList = new List<Human>();
+public List<IInternetConnection> List_webconnections(List<Robot> _robotsList, List<Human> _humansList)
+    {
+        List<IInternetConnection> _webList = new();
+        foreach (Robot robot in _robotsList)
+        {
+            if (robot is IInternetConnection)
+                _webList.Add((IInternetConnection)robot);
+            else continue;
+        }
+        foreach(Human human in _humansList)
+        {
+            if(human is IInternetConnection)
+                _webList.Add((IInternetConnection)human);
+            else continue;
+        }
+        return _webList;
+    }
+public Factory(List<Robot> robotsList, List<Human> humansList)
+{
+    _robotsList = robotsList;
+    _humansList = humansList;
+}
+public int Totalcost_Robots()
+{
+    int totalcost = 0;
+    foreach (Robot robot in _robotsList)
+    {
+        totalcost += robot.AnnualCost();
+    }
+    return totalcost;
+}
+public int Totalcost_Humans()
+{
+    int totalcost = 0;
+    foreach (Human human in _humansList)
+    {
+        totalcost += human.Cost(human.Months);
+    }
+    return totalcost;
+}
+public bool WebConnection_Humans()
+    {
+        bool webConnection = true;
+        foreach (Human human in _humansList)
+        {
+            if (human is OfficeWorker)
+            {   
+                OfficeWorker officeWorker = human as OfficeWorker;
+                if (officeWorker.InternetConnection() == false)
+                {
+                    webConnection = false;
+                }
+                else {
+                    continue;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return webConnection;
+    }
+    public bool WebConnection_Robots()
+    {
+        bool webConnection = true;
+        foreach (Robot robot in _robotsList)
+        {
+            if (robot is ComputerRobot)
+            {
+                ComputerRobot computerRobot = robot as ComputerRobot;
+                if (computerRobot.InternetConnection() == false)
+                {
+                    webConnection = false;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return webConnection;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Robot> _robotsList = new () {new ComputerRobot(true), new ComputerRobot(true),new MechanicalRobot()};
+        List<Human> _humansList = new() { new Handworker("Krzysztof",4), new Handworker("Jared", 7), new OfficeWorker("Koks", 5,true), new OfficeWorker("Maciek", 5, true) };
+        Factory Test = new Factory(_robotsList, _humansList);
+        foreach(Human human in _humansList)
+        {
+            Console.WriteLine(human.To_String());
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Connection status for humans:{Test.WebConnection_Humans()}");
+        Console.WriteLine($"Connection status for robots:{Test.WebConnection_Robots()}");
+        Console.WriteLine();
+        Console.WriteLine($"Total cost of humans:{Test.Totalcost_Humans()}");
+        Console.WriteLine($"Total cost of robots:{Test.Totalcost_Robots()}");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine($"{Test.List_webconnections(_robotsList, _humansList)[1].InternetConnection()}");
+    }
+}
